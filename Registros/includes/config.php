67eb1c832063e584e99ec2cee294470456ddb309
YakiVS <104659872+YakiVS@users.noreply.github.com>
2024-02-17 09:27:36 -0500

    Update config.php

--- Registros/includes/config.php
+++ Registros/includes/config.php
@@ -1,7 +1,7 @@
 <?php
 
 /* Realizamos la conexion a la base de datos. */
-$conn = new mysqli('localhost','eltecico_registrosu','itec@andahuaylas', 'eltecico_registro1');
+$conn = new mysqli('localhost','registros','itec', 'itec');
 
 
 if ($conn->connect_error) {
@@ -12,16 +12,4 @@ if ($conn->connect_error) {
 
 <?php
  
-    /*===========================================
-    | Datos del servidor - Data of the server |
-    ===========================================*/
-    //local
-    /*const SERVER="localhost";
-    const DB="eltecico_registro1";
-    const USER="eltecico_registrosu"; 
-    const PASS="itec@andahuaylas"; 
-    //server
-    //Only modify the following line in case the database manager is not MySQL
-    // Solo modificar la siguiente lикnea en caso el gestor de base de datos no sea MySQL
-    const SGBD="mysql:host=".SERVER.";dbname=".DB;*/
  ?>
